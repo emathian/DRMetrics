dc98eeb2cf24c7281cf0c5d134c1373caa5c9911
Emi <emilie.mathian@insa-lyon.fr>
2019-08-07 10:33:32 +0200

    Comment installation process

--- DR_Method/Dimensionality_reduction_comparison_metrics.R
+++ DR_Method/Dimensionality_reduction_comparison_metrics.R
@@ -1,19 +1,19 @@
-
-if(!require(foreach)) install.packages("foreach",repos = "https://CRAN.R-project.org/package=foreach")
-if(!require(doParallel)) install.packages("doParallel",repos = "https://CRAN.R-project.org/package=doParallel")
-if(!require(ggplot2)) install.packages("ggplot2",repos = "https://CRAN.R-project.org/package=ggplot2")
-if(!require(RColorBrewer)) install.packages("RColorBrewer",repos = "https://CRAN.R-project.org/package=RColorBrewer")
-if(!require(plotly)) install.packages("plotly",repos = "https://CRAN.R-project.org/package=plotly")
-if(!require(spatial)) install.packages("spatial",repos = "https://CRAN.R-project.org/package=plotly")
-if(!require(raster)) install.packages("raster",repos = "https://CRAN.R-project.org/package=raster")
-if(!require(spdep)) install.packages("spdep",repos = "https://CRAN.R-project.org/package=spdep")
-if(!require(FNN)) install.packages("FNN",repos = "https://CRAN.R-project.org/package=FNN")
-if(!require(viridis)) install.packages("viridis",repos = "https://CRAN.R-project.org/package=viridis")
-if(!require(venn)) install.packages("venn",repos = "https://CRAN.R-project.org/package=venn")
-if(!require(VennDiagram)) install.packages("VennDiagram",repos = "https://CRAN.R-project.org/package=VennDiagram")
-if(!require(latex2exp)) install.packages("latex2exp",repos = "https://CRAN.R-project.org/package=latex2exp")
-
-
+# 
+# if(!require(foreach)) install.packages("foreach",repos = "https://CRAN.R-project.org/package=foreach")
+# if(!require(doParallel)) install.packages("doParallel",repos = "https://CRAN.R-project.org/package=doParallel")
+# if(!require(ggplot2)) install.packages("ggplot2",repos = "https://CRAN.R-project.org/package=ggplot2")
+# if(!require(RColorBrewer)) install.packages("RColorBrewer",repos = "https://CRAN.R-project.org/package=RColorBrewer")
+# if(!require(plotly)) install.packages("plotly",repos = "https://CRAN.R-project.org/package=plotly")
+# if(!require(spatial)) install.packages("spatial",repos = "https://CRAN.R-project.org/package=plotly")
+# if(!require(raster)) install.packages("raster",repos = "https://CRAN.R-project.org/package=raster")
+# if(!require(spdep)) install.packages("spdep",repos = "https://CRAN.R-project.org/package=spdep")
+# if(!require(FNN)) install.packages("FNN",repos = "https://CRAN.R-project.org/package=FNN")
+# if(!require(viridis)) install.packages("viridis",repos = "https://CRAN.R-project.org/package=viridis")
+# if(!require(venn)) install.packages("venn",repos = "https://CRAN.R-project.org/package=venn")
+# if(!require(VennDiagram)) install.packages("VennDiagram",repos = "https://CRAN.R-project.org/package=VennDiagram")
+# if(!require(latex2exp)) install.packages("latex2exp",repos = "https://CRAN.R-project.org/package=latex2exp")
+# 
+# 
 
 library(foreach)
 library(doParallel)
@@ -30,7 +30,7 @@ library(latex2exp)
 library(VennDiagram)
 library(venn)
 library(viridis)
-
+library(foreach)
 source("../DR_Method/SEQ_DIFF.R") 
 source("../DR_Method/CP.R") 
 source("../DR_Method/MORAN_I.R")
